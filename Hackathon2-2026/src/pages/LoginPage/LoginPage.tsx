b1748a155af87a52a950df5dceae8f1299718ef8
urlony <olaybarchs@gmail.com>
2026-03-15 16:40:40 +0800

    Login and Register Page

--- Hackathon2-2026/src/pages/LoginPage/LoginPage.tsx
+++ Hackathon2-2026/src/pages/LoginPage/LoginPage.tsx
@@ -1,11 +1,35 @@
 import './logindesign.css'
-function LoginPage()
-{
-    return(
-        <>
-        
-        </>
+import { useState } from 'react'
+
+function LoginPage() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const TryLogin = () => {
+        console.log('Username:', username);
+        console.log('Password:', password);
+    }
+
+    return (
+        <div className="loginpage-wrapper">
+            <div className="loginpage-container">
+                <svg className='box-icon'></svg>
+                <h1>Inventory</h1>
+                <p>Admin Login</p>
+
+                <div className="input-group">
+                    <p>Username</p>
+                </div>
+                    <input onChange={(e) => setUsername(e.target.value)}value={username}type="text"id="username"placeholder="Enter username"className='input-login'/>
+                <div className="input-group">
+                    <p>Password</p>
+                </div>
+                    <input onChange={(e) => setPassword(e.target.value)}value={password}type="password"id="password"placeholder="Enter password"/>
+                        <button className="login-button" onClick={TryLogin}>Login</button>
+                <a href="/register" className="register-link">Don't have an account? Register Here</a>
+            </div>
+        </div>
     )
 }
 
-export default LoginPage;
+export default LoginPage
