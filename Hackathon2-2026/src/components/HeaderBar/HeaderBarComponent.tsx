71d49d5535710efeacc25531794ab71f4acb4a16
eundar <deanpevamir@gmail.com>
2026-03-15 16:19:44 +0800

    Finished StatsCard Dashboard HeaderBar

--- Hackathon2-2026/src/components/HeaderBar/HeaderBarComponent.tsx
+++ Hackathon2-2026/src/components/HeaderBar/HeaderBarComponent.tsx
@@ -1,10 +1,17 @@
-function HeaderBarComponent()
-{
-    return(
-        <>
-        
-        </>
-    )
+import "./headerbar.css";
+function HeaderBarComponent() {
+  return (
+    <div className="header">
+      <div className="leftside">
+        <div className="logo">
+          <p>Inventory</p>
+        </div>
+        <p>Purchase</p>
+      </div>
+
+      <button className="additem">+ Add Item</button>
+    </div>
+  );
 }
 
-export default HeaderBarComponent()
+export default HeaderBarComponent;
